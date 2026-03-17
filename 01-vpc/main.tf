15bd2aa3cefa6363263071af2ce59b46b16a992e
bhukyadevops@gmail.com <bhukyadevops@gmail.com>
2026-03-17 12:19:43 +0530

    action

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "../../terraform-aws-vpc"
+    source = "../../terraforme-aws-vpc"
     project = var.project
     environment = var.environment
     is_peering_required = true
